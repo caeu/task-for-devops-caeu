04286823890a178bb4fa98fd0398873eb330cadf
Caesar <caeusr@gmail.com>
2022-02-07 05:44:03 +0100

    Basic structure for Flask relay app

---
+++ ANSWERS.md
@@ -0,0 +1,5 @@
+### Task 1 design and comments
+`Flask` is python micro webapplication that is light, fast and relatively simple to work with. It seems fit for this task.
+Since there are no actual webpages served in this application, only a single python file is created, i.e., no template or static directories.
+
+To run this app, the `requests` library will be imported as well.
---
+++ app.py
@@ -0,0 +1,35 @@
+import requests
+from flask import Flask
+
+# Source api
+Dcbtube_url = 'https://menu.dckube.scilifelab.se/api/'
+
+
+# Instantiate the flask app
+app = Flask(__name__)
+
+
+# Function for backend processing can be 
+# extended or moved to another source file
+def backend(content):
+    '''
+    Receives object of type dict
+    '''
+    # processing if needed
+    
+    return (content)
+
+
+# main route
+# using 'path' type to allow '/' for subpaths
+@app.route("/<path:uri>")
+def relay(uri):
+    response = requests.get(Dcbtube_url + uri)
+    content = response.json()
+    content = backend(content)
+    
+    return content
+
+
+if __name__ == "__main__":
+    app.run()
