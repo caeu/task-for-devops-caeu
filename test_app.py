816c4c4c0aa51dd14ad7eaeaeaff14c9c126a854
Caesar <caeusr@gmail.com>
2022-02-07 07:49:12 +0100

    chore: Fix test cases based on Flask docs

--- test_app.py
+++ test_app.py
@@ -1,34 +1,40 @@
 import unittest
-import requests
+from app import app
     
 class TestRelay(unittest.TestCase):
-    # Default Flask
-    ApiUrl = "http://127.0.0.1:5000/"
     
-    # test response ok
+    # required for app testing
+    app.testing = True
+    
     def test_res_ok(self):
-        res = requests.get(self.ApiUrl + "restaurant")
-        self.assertEqual(res.status_code, 200)
-        
+        with app.test_client() as c:
+            res = c.get("/restaurant")
+            self.assertEqual(res.status_code, 200)
+
     # test response not_ok
     def test_res_nok1(self):
-        res = requests.get(self.ApiUrl)
-        self.assertNotEqual(res.status_code, 200)
-        
-    def test_res_nok2(self):
-        res = requests.get(self.ApiUrl + "restaurant/not_a_restaurant_really")
-        self.assertNotEqual(res.status_code, 200)
-        
-    # test json 200
-    def test_res_json(self):
-        res = requests.get(self.ApiUrl + "restaurant")
-        self.assertEqual(res.headers.get('content-type'), 'application/json')
-        
-    # test json NOT 200
-    def test_res_json_err(self):
-        res = requests.get(self.ApiUrl + "restaurant/not_a_restaurant_really")
-        self.assertEqual(res.headers.get('content-type'), 'application/json')
+        with app.test_client() as c:
+            res = c.get()
+            self.assertNotEqual(res.status_code, 200)
     
+           
+    def test_res_nok2(self):
+        with app.test_client(self) as c:
+            res = c.get("restaurant/not_a_restaurant_really")
+            self.assertNotEqual(res.status_code, 200)
+    
+    # test json 200       
+    def test_res_json(self):
+        with app.test_client(self) as c:
+            res = c.get("restaurant")
+            self.assertEqual(res.headers.get("content-type"), "application/json")
+    
+    # test json Not 200        
+    def test_res_json_err(self):
+        with app.test_client(self) as c:
+            res = c.get("restaurant/not_a_restaurant_really")
+            self.assertEqual(res.headers.get("content-type"), "application/json")
+
         
 if __name__ == "__main__":
         unittest.main()
